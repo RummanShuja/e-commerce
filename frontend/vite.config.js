a7d5cd59ae0818194ef94996f94bee616c6a948a
RummanShuja <shujarumman@gmail.com>
2025-11-06 05:40:26 +0530

    fix: Resolve Vercel build error

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,9 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),
+  plugins: [react({
+    jsxRuntime: 'automatic'
+  }),
     tailwindcss(),
   ],
   server: {port:5173}
